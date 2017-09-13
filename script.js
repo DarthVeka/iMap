390bc9551341162840827e489e39be5e22b1515a
Darth.Veka <vedran.von.vitez@gmail.com>
2017-09-13 14:56:29 +0200

    Added functionality on click dynamically creation of  redion details inside html, and some optimization and bug fixing of .js file

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ $(document).ready(function () {
         let selectedRegionName = $('.selected-region-name');
         let swapBtn = $('#swap-map');
         let compareBtn = $('#compare');
+        let genDataCont = $('.generated-data');
+        let genData = $('.dynamic-region');
         let comparison = false;
         let selectedRegions = [];
 
@@ -116,25 +118,54 @@ $(document).ready(function () {
                 selected.toggleClass('selected');
                 let index = selectedRegions.indexOf(selected[0].id);
 
-                (index > -1) ? selectedRegions.splice(index, 1) : selectedRegions.push(selected[0].id);
+                (index > -1) 
+                    ? (selectedRegions.splice(index, 1), removeSelectedRegion(selected))
+                    : (selectedRegions.push(selected[0].id), getSelectedData(selected));
 
                 // Add region name to html
                 let oldText = selectedRegionName.text();
                 let newRegion = selected.attr('title');
                 let indexOfRegion = oldText.indexOf(newRegion);
                 // If the region is already added remove it from the list
-                (indexOfRegion == -1) ? (oldText += ' ', oldText += newRegion ) : (oldText = oldText.split(newRegion).join(''));
-               
+                (indexOfRegion == -1)
+                    ? (oldText += ' ', oldText += newRegion) 
+                    : (oldText = oldText.split(newRegion).join(''));
+
                 selectedRegionName.text(oldText);
 
                 console.log(selectedRegions);
             } else {
-                console.log(selected.attr('title'));
                 regions.removeClass('selected');
+                genDataCont.empty();
+                getSelectedData(selected);
                 selected.addClass('selected');
                 selectedRegionName.text(selected.attr('title'));
             }
         }
+
+        function getSelectedData(selected) {
+            let link = './zupanije.json';
+            let selectedId = selected.attr('id');
+
+            $.get(link, function (data) {
+                for(let i = 0; i < data.zupanije.length; i++) {
+                    if(selectedId === data.zupanije[i].id) {
+                        let htmlString = "<div class='"+ data.zupanije[i].id +" dynamic-region '><img src='"+ data.zupanije[i].grb +"' alt='"+ data.zupanije[i].name +"'>";
+                        htmlString += "<p>"+ data.zupanije[i].name +"</p></div>";
+
+                        genDataCont.append(htmlString);
+                    }
+                }
+            });
+        }
+
+        function removeSelectedRegion(selected) {
+            let selectedId = '.';
+            selectedId += selected.attr('id');
+            $(selectedId).remove();
+        }
+
+
     };
     document.onload = init();
 });
